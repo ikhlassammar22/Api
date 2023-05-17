dd507b99445fc2fc8b95b23d56b009818bec82a1
ammar ikhlass <ikhlassammar7@gmail.com>
2023-05-17 14:57:01 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import User from "./components/User";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    await fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data))
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h5>React Crud Using Jsonplaceholder</h5>
+      {users.map((user) => (
+        <User id={user.id} key={user.id} name={user.name} email={user.email} />
+      ))}
     </div>
   );
 }
